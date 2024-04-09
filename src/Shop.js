6b171f8288c58a3103fd8f0a616cd221d509135a
shatakshisachan051 <shatakshisachan1017@gmail.com>
2024-04-09 14:03:53 +0530

    updated store

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
+import { useSelector } from 'react-redux';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+    const balance = useSelector(state => state.amount )
+    const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators,dispatch);
   return (
     <div>
-         <h2>Buy Adidas Shoes 50 Rs</h2>
-        <button className="btn-primary mx-2">-</button>
-         Add to cart
-        <button className="btn-primary mx-2">+</button>
+         <h2>Deposit/Withdraw Money</h2>
+        <button className="btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+         Update Balance ({balance})
+        <button className="btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
       
     </div>
   )
